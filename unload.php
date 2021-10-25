5ec323a2ca2987ed1d0fd0f919511adc00c76910
Roman <61219581+Roman144000@users.noreply.github.com>
2021-10-25 17:46:26 +0300

    Update unload.php

--- unload.php
+++ unload.php
@@ -1,66 +1,76 @@
 <?php
+
 /**
-* Plugin Name: unload
-* Description: Плагин выгрузки товаров из woocommerce
-* Version: 1.0.0
-* Author: Roman
-*/
+ * Plugin Name: unload
+ * Description: Плагин выгрузки товаров из woocommerce
+ * Version: 1.0.0
+ * Author: Roman
+ */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-add_action('admin_menu', function(){
-    add_menu_page( 'Выгрузка каталога', 'Выгрузка', 'manage_options', 'unload', 'page_view', '', 6 );
-} );
+add_action('admin_menu', function () {
+    add_submenu_page('woocommerce', 'Выгрузка каталога', 'Выгрузка', 'manage_options', 'unload', 'page_view');
+});
 
-function page_view() {
+function page_view()
+{
 ?>
 
-	<h2><?php echo get_admin_page_title() ?></h2>
+    <h2><?php echo get_admin_page_title() ?></h2>
 
-	<form method="post">
-		<input type="hidden" name="xml" value="1">
+    <form method="post">
+        <input type="hidden" name="xml" value="1">
 
-		<?submit_button('Получить xml')?>
-	</form>
+        <div style="margin-bottom: 30px;">
+            <select name="exist">
+                <option disabled selected>Наличие товара</option>
+                <option>В наличии</option>
+            </select>
+        </div>
+
+        <div>
+            <span>Цена</span>
+            <input type="text" name="pricefrom"> - <input type="text" name="pticeto">
+        </div>
+
+        <? submit_button('Получить xml') ?>
+    </form>
 
 <?
 }
 
-if(isset($_POST['xml'])) {
-	global $wpdb;
+if (isset($_POST['xml'])) {
+    global $wpdb;
 
-	$products = $wpdb->get_results("SELECT product_id FROM info_wc_product_meta_lookup");
+    $products = $wpdb->get_results("SELECT ID, post_title FROM $wpdb->posts WHERE post_type='product'");
 
     $xml = new DOMDocument('1.0', 'UTF-8');
 
-	$xml->formatOutput = true;
+    $xml->formatOutput = true;
 
     $catalog = $xml->createElement("каталог");
-	
+
     $catalog = $xml->appendChild($catalog);
 
-	foreach($products as $product => $value) {
+    foreach ($products as $p) {
 
-		$id = $value->product_id;
+        $product = $xml->createElement("товар");
 
-		$name = $wpdb->get_var($wpdb->prepare("SELECT post_title FROM info_posts WHERE ID = %s", $id));
+        $product = $catalog->appendChild($product);
 
-		$product = $xml->createElement("товар");
+        $product_id = $xml->createElement("ид", $p->ID);
 
-		$product = $catalog->appendChild($product);
+        $product_id = $product->appendChild($product_id);
 
-		$product_id = $xml->createElement("ид", $id);
+        $product_name = $xml->createElement("название", $p->post_title);
 
-		$product_id = $product->appendChild($product_id);
+        $product_name = $product->appendChild($product_name);
+    }
 
-		$product_name = $xml->createElement("название", $name);
+    header('Content-Type: text/xml');
 
-		$product_name = $product->appendChild($product_name);
+    header('Content-Disposition: attachment; filename="catalog.xml"');
 
-	}
-	
-	header("Content-type:text/xml; charset=utf-8");
-	header("Content-Disposition: attachment; filename=catalog.xml");
-	
     $xml->save('php://output');
 }
